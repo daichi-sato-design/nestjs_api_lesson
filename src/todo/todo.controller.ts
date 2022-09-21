7682e47a327a164fdd42c1399f3ce1b4a53cd0b6
daichi-sato-design <daichisato.design@gmail.com>
2022-09-21 22:57:01 +0900

    todoのサービスとコントローラーを実装

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,70 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Patch,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Query,
+  ParseIntPipe,
+  UseGuards,
+  Req,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { TodoService } from './todo.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from '@prisma/client';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  getTasks(
+    @Req() req: Request,
+    @Query('page', ParseIntPipe) page: number,
+    @Query('limit', ParseIntPipe) limit: number,
+  ): Promise<Task[]> {
+    return this.todoService.getTasks(req.user.id, page - 1, limit);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':id')
+  getTaskById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<Task> {
+    return this.todoService.getTaskById(req.user.id, taskId);
+  }
+
+  @Post()
+  createTask(@Req() req: Request, @Body() dto: CreateTaskDto): Promise<Task> {
+    return this.todoService.createTask(req.user.id, dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch(':id')
+  updateTaskById(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+    @Body() dto: UpdateTaskDto,
+  ): Promise<Task> {
+    return this.todoService.updateTask(req.user.id, taskId, dto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteTask(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) taskId: number,
+  ): Promise<void> {
+    return this.todoService.deleteTaskById(req.user.id, taskId);
+  }
+}
